7ad66d5665168d4ff5a537f4709d2988989429dd
biminkoju <68364413+biminkoju@users.noreply.github.com>
2021-04-13 20:17:04 +0545

    base code

---
+++ Bot.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System.IO;
+using DSharpPlus.EventArgs;
+using Newtonsoft.Json;
+
+namespace charpbot
+{
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+       
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            var json = string.Empty;
+
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = sr.ReadToEnd();
+
+            var configJson = JsonConvert.DeserializeObject<configjson>(json);
+
+            var config = new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+            };
+
+
+            Client = new DiscordClient(config);
+
+            Client.Ready += OnClientReady;
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { configJson.Prefix },
+                EnableDms = false,
+                EnableMentionPrefix = true,
+                DmHelp = true,
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            await Client.ConnectAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task OnClientReady(object sender, ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace charpbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("i am live");
+            var bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}
